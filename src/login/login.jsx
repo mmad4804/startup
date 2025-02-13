6e428f5ecd2d405a411613df56b809250c9a0cf2
mmad4804 <madariagamica@gmail.com>
2025-02-13 09:19:37 -0700

    Ported login page over

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import './login.css';
 
 export function Login() {
-  return (
-    <main>
-      <div>login displayed here</div>
-    </main>
-  );
+    return (
+        <main className="main_login">
+            <h2 className="login-title">Welcome to TuneShare!</h2>
+            <p id="login-instructions">Login or create an account to get started</p>
+            <form className="login-form" action="home.html" method="post">
+                <section className="login-section">
+                    <div className="form-group">
+                        <label className="login-label" for="username">Username:</label>
+                        <input className="textbox" type="text" name="username" id="username" />
+                    </div>
+                    <div class="form-group">
+                        <label className="login-label" for="password">Password:</label>
+                        <input className="textbox" type="password" name ="password" id="password" />
+                    </div>
+                    <br />
+                    <div className="buttons">
+                        <input id="login-button" type="submit" value="Login" />
+                        <input id="register-button" type="submit" value="Register" />
+                    </div> 
+                </section>
+            </form>
+        </main>
+    );
 }
