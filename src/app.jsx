d855f2f421ab4e51b7173fa9a5925a8d14244c07
mmad4804 <madariagamica@gmail.com>
2025-02-12 20:59:51 -0700

    Created the router

--- src/app.jsx
+++ src/app.jsx
@@ -1,33 +1,40 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Home } from './home/home';
+import { Feed } from './feed/feed';
+import { Saved } from './saved/saved';
 
 export default function App() {
   return (
-    <div>
-        <body>
-            <header>
-                <img id="logo" src="logo.png" alt="TuneShare Logo"/>
-                <h1>TuneShare</h1>
+    <BrowserRouter>
+        <div>
+            <body>
+                <header>
+                    <img id="logo" src="logo.png" alt="TuneShare Logo"/>
+                    <h1>TuneShare</h1>
 
-                <nav>
-                    <menu>
-                        <li className="nav-bar-item" id="login-link"><a href="index.html">Login</a></li>
-                        <li className="nav-bar-item"><a href="home.html">Home</a></li>
-                        <li className="nav-bar-item"><a href="feed.html">Feed</a></li>
-                        <li className="nav-bar-item"><a href="saved.html">Saved</a></li>
-                    </menu>
-                </nav>
-            </header>
+                    <nav>
+                        <menu>
+                            <li className="nav-bar-item" id="login-link"><a href="index.html">Login</a></li>
+                            <li className="nav-bar-item"><a href="home.html">Home</a></li>
+                            <li className="nav-bar-item"><a href="feed.html">Feed</a></li>
+                            <li className="nav-bar-item"><a href="saved.html">Saved</a></li>
+                        </menu>
+                    </nav>
+                </header>
 
-            <main>App components go here</main>
+                <main>App components go here</main>
 
-            <footer>
-                <span className="author">Micaela Madariaga</span>
-                <br />
-                <a id="github" href="https://github.com/mmad4804/startup.git">Github</a>
-            </footer>
-        </body>
-    </div>
+                <footer>
+                    <span className="author">Micaela Madariaga</span>
+                    <br />
+                    <a id="github" href="https://github.com/mmad4804/startup.git">Github</a>
+                </footer>
+            </body>
+        </div>
+    </BrowserRouter>
   )
 }
