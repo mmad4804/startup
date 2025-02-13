3f9adbad445c50d298670517d0bda63c3f1582cc
mmad4804 <madariagamica@gmail.com>
2025-02-13 09:31:44 -0700

    Ported home page over

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './home.css';
 
 export function Home() {
-  return (
-    <main>
-      <div>home page displayed here</div>
-    </main>
-  );
+    return (
+        <main>
+            <button className = "add-song-button" type="button">+</button>
+            <h2 id="add-song-title">Add Your Song of the Day!</h2>
+        </main>
+    );
 }
