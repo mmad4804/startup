3c6dd3a26fc272f2e5363596d6ae9fc3b506b857
mmad4804 <madariagamica@gmail.com>
2025-02-25 13:20:25 -0700

    Reformatted buttons

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -1,28 +1,37 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 
-import './login.css';
+import "./login.css";
+import { Home } from "../home/home";
 
 export function Authenticated(props) {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    function logout() {
-        localStorage.removeItem('userName');
-        console.log('Logging out');
-        props.onLogout();
-    }
+  function logout() {
+    localStorage.removeItem("userName");
+    props.onLogout();
+  }
 
-    return (
-        <div>
-            <div className='playerName'>{props.userName}</div>
-            <Button variant='primary' onClick={() => navigate('/home')}>
-                Home
-            </Button>
-            <Button variant='secondary' onClick={() => logout()}>
-                Logout
-            </Button>
-        </div>
-    )
+  return (
+    <div>
+      <div className="buttons">
+        <Button
+          variant="primary"
+          id="register-button"
+          onClick={() => navigate("/home")}
+        >
+          Home
+        </Button>
+        <Button
+          variant="secondary"
+          id="register-button"
+          onClick={() => logout()}
+        >
+          Logout
+        </Button>
+      </div>
+    </div>
+  );
 }
