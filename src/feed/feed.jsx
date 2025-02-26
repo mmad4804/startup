aac30fa5bb0b364486589c67ffe0374b00f3907c
mmad4804 <madariagamica@gmail.com>
2025-02-25 17:18:55 -0700

    Added interactivity to saved songs page

--- src/feed/feed.jsx
+++ src/feed/feed.jsx
@@ -1,47 +1,77 @@
-import React from 'react';
-import './feed.css';
+import React from "react";
+import "./feed.css";
 
 export function Feed() {
-    return (
-        <main className="main_feed">
-            <h2 id="feed-title">See What Others Are Posting</h2>
-            <ul className="feed-ul">
-                <section className="feed-section">
-                    <table className="feed-table">
-                        <tr className="feed-row">
-                            <td class="button-section">
-                                <button class="play-button" type="button"><span><img className="play-button-image" src="play-con.webp"></img></span></button>
-                            </td>
-                            <td class="song-info">
-                                <table className="feed-table">
-                                    <tr className="feed-row"><p class="username">JohnnyLingo674</p></tr>
-                                    <tr className="feed-row"><h3 class="song-title">Range Rover</h3></tr>
-                                    <tr className="feed-row"><p class="artist">Ben Rector</p></tr>
-                                </table>
-                            </td>
-                        </tr>
-                    </table>
-                    <button class="saved-songs-button" type="button">Add to Saved Songs</button>
-                </section>
-                <br />
-                <section className="feed-section">
-                    <table className="feed-table">
-                        <tr className="feed-row">
-                            <td class="button-section">
-                                <button class="play-button" type="button"><span><img className="play-button-image" src="play-con.webp"></img></span></button>
-                            </td>
-                            <td class="song-info">
-                                <table className="feed-table">
-                                    <tr className="feed-row"><p class="username">username</p></tr>
-                                    <tr className="feed-row"><h3 class="song-title">Song Name</h3></tr>
-                                    <tr className="feed-row"><p class="artist">Artist</p></tr>
-                                </table>
-                            </td>
-                        </tr>
-                    </table>
-                    <button class="saved-songs-button" type="button">Add to Saved Songs</button>
-                </section>
-            </ul>
-        </main>
-    );
+  return (
+    <main className="main_feed">
+      <h2 id="feed-title">See What Others Are Posting</h2>
+      <ul className="feed-ul">
+        <section className="feed-section">
+          <table className="feed-table">
+            <tr className="feed-row">
+              <td class="button-section">
+                <button class="play-button" type="button">
+                  <span>
+                    <img
+                      className="play-button-image"
+                      src="play-con.webp"
+                    ></img>
+                  </span>
+                </button>
+              </td>
+              <td class="song-info">
+                <table className="feed-table">
+                  <tr className="feed-row">
+                    <p class="username">JohnnyLingo674</p>
+                  </tr>
+                  <tr className="feed-row">
+                    <h3 class="song-title">Range Rover</h3>
+                  </tr>
+                  <tr className="feed-row">
+                    <p class="artist">Ben Rector</p>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table>
+          <button class="saved-songs-button" type="button">
+            Add to Saved Songs
+          </button>
+        </section>
+        <br />
+        <section className="feed-section">
+          <table className="feed-table">
+            <tr className="feed-row">
+              <td class="button-section">
+                <button class="play-button" type="button">
+                  <span>
+                    <img
+                      className="play-button-image"
+                      src="play-con.webp"
+                    ></img>
+                  </span>
+                </button>
+              </td>
+              <td class="song-info">
+                <table className="feed-table">
+                  <tr className="feed-row">
+                    <p class="username">username</p>
+                  </tr>
+                  <tr className="feed-row">
+                    <h3 class="song-title">Song Name</h3>
+                  </tr>
+                  <tr className="feed-row">
+                    <p class="artist">Artist</p>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table>
+          <button class="saved-songs-button" type="button">
+            Add to Saved Songs
+          </button>
+        </section>
+      </ul>
+    </main>
+  );
 }
--- src/saved/saved.jsx
+++ src/saved/saved.jsx
@@ -1,59 +1,112 @@
-import React from 'react';
-import './saved.css';
+import React from "react";
+import "./saved.css";
 
 export function Saved() {
+  const [savedSongs, setSavedSongs] = React.useState([]);
+
+  //Make sure spotify web api gives json song data
+  React.useEffect(() => {
+    const savedSongText = localStorage.getItem("savedSongText");
+    if (savedSongText) {
+      setSavedSongs(JSON.parse(savedSongText));
+    }
+  }, []);
+
+  const songInfo = [];
+  if (savedSongs.length) {
+    for (const [i, song] of savedSongs.entries()) {
+      songInfo.push(
+        <section className="song" key={i}>
+          <tr className="song-row">
+            <td>
+              <button className="play-button" type="button">
+                <span>
+                  <img className="play-button-image" src="play-con.webp"></img>
+                </span>
+              </button>
+            </td>
+            <td>
+              <table className="song-table">
+                <tr className="song-row">
+                  <h3 className="song-title">{song.title}</h3>
+                </tr>
+                <tr className="song-row">
+                  <p className="artist">{song.artist}</p>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </section>
+      );
+    }
+  } else {
+    songInfo.push(
+      <tr className="song-row" key={0}>
+        <h3 className="song-title">No saved songs</h3>
+      </tr>
+    );
+  }
+
   return (
     <main className="main_saved">
-        <h2 className="main-title">Your Saved Songs</h2>
-        <ul className="saved-songs">
-            <section className="song">
+      <h2 className="main-title">Your Saved Songs</h2>
+      <ul className="saved-songs">
+        {songInfo}
+        <br></br>
+        <section className="song">
+          <table className="song-table">
+            <tr className="song-row">
+              <td>
+                <button className="play-button" type="button">
+                  <span>
+                    <img
+                      src="play-con.webp"
+                      className="play-button-image"
+                    ></img>
+                  </span>
+                </button>
+              </td>
+              <td>
                 <table className="song-table">
-                    <tr className="song-row">
-                        <td>
-                            <button className="play-button" type="button"><span><img className="play-button-image" src="play-con.webp"></img></span></button>
-                        </td>
-                        <td>
-                            <table className="song-table">
-                                <tr className="song-row"><h3 className="song-title">Heat Waves</h3></tr>
-                                <tr className="song-row"><p className="artist">Glass Animals</p></tr>
-                            </table>
-                        </td>
-                    </tr>
+                  <tr className="song-row">
+                    <h3 className="song-title">Range Rover</h3>
+                  </tr>
+                  <tr className="song-row">
+                    <p className="artist">Ben Rector</p>
+                  </tr>
                 </table>
-            </section>
-            <br></br>
-            <section className="song">
+              </td>
+            </tr>
+          </table>
+        </section>
+        <br></br>
+        <section className="song">
+          <table className="song-table">
+            <tr className="song-row">
+              <td>
+                <button className="play-button" type="button">
+                  <span>
+                    <img
+                      src="play-con.webp"
+                      className="play-button-image"
+                    ></img>
+                  </span>
+                </button>
+              </td>
+              <td>
                 <table className="song-table">
-                    <tr className="song-row">
-                        <td>
-                            <button className="play-button" type="button"><span><img src="play-con.webp" className="play-button-image"></img></span></button>
-                        </td>
-                        <td>
-                            <table className="song-table">
-                                <tr className="song-row"><h3 className="song-title">Range Rover</h3></tr>
-                                <tr className="song-row"><p className="artist">Ben Rector</p></tr>
-                            </table>
-                        </td>
-                    </tr>
+                  <tr className="song-row">
+                    <h3 className="song-title">Stargazing</h3>
+                  </tr>
+                  <tr className="song-row">
+                    <p className="artist">Miles Smyth</p>
+                  </tr>
                 </table>
-            </section>
-            <br></br>
-            <section className="song">
-                <table className="song-table">
-                    <tr className="song-row">
-                        <td>
-                            <button className="play-button" type="button"><span><img src="play-con.webp" className="play-button-image"></img></span></button>
-                        </td>
-                        <td>
-                            <table className="song-table">
-                                <tr className="song-row"><h3 className="song-title">Stargazing</h3></tr>
-                                <tr className="song-row"><p className="artist">Miles Smyth</p></tr>
-                            </table>
-                        </td>
-                    </tr>
-                </table>
-            </section>
-        </ul>
+              </td>
+            </tr>
+          </table>
+        </section>
+      </ul>
     </main>
   );
 }
