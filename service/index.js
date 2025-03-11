da5e79d69925ba6fc5b5312ad9d16fe209764b35
mmad4804 <madariagamica@gmail.com>
2025-03-10 21:34:14 -0600

    Added https endpoints

--- service/index.js
+++ service/index.js
@@ -5,7 +5,95 @@ const uuid = require("uuid");
 const app = express();
 
 const authCookieName = "token";
+
 const users = [];
 // const savedSongs?
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
-app.listen(4000);
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static("public"));
+
+var apiRouter = express.Router();
+app.use("/api", apiRouter);
+
+//CreateAuth a new user
+apiRouter.post("/auth/register", async (req, res) => {
+  if (await findUser("username", req.body.username)) {
+    res.status(409).send({ msg: "Username already exists" });
+  } else {
+    const user = await createUser(req.body.username, req.body.password);
+
+    setAuthCookie(res, user.token);
+    res.send({ username: user.username });
+  }
+});
+
+//GetAuth login an existing user
+apiRouter.post("/auth/login", async (req, res) => {
+  const user = await findUser("username", req.body.username);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      user.token = uuid.v4();
+      setAuthCookie(res, user.token);
+      res.send({ username: user.username });
+      return;
+    }
+  }
+  res.status(401).send({ msg: "Unauthorized" });
+});
+
+//DeleteAuth to logout a user
+apiRouter.delete("/auth/logout", async (req, res) => {
+  const user = await findUser("token", req.cookies[authCookieName]);
+  if (user) {
+    delete user.token;
+  }
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+//Middleware to verify if a user is authenticated
+const verifyAuth = async (req, res, next) => {
+  const user = await findUser("token", req.cookies[authCookieName]);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: "Unauthorized" });
+  }
+};
+
+//Default error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+//Helper function to create a new user
+async function createUser(username, password) {
+  const user = {
+    username: username,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  users.push(user);
+  return user;
+}
+
+//Helper function to find a user by a property
+async function findUser(field, value) {
+  if (!value) return null;
+
+  return users.find((user) => user[field] === value);
+}
+
+function setAuthCookie(res, token) {
+  res.cookie(authCookieName, token, {
+    secure: true,
+    httpOnly: true,
+    sameSite: "strict",
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
