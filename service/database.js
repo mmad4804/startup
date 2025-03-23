68ae08f0c01d2e41626dc0515e93b448b13b5cd3
mmad4804 <madariagamica@gmail.com>
2025-03-22 21:27:25 -0600

    Added database file

---
+++ service/database.js
@@ -0,0 +1,53 @@
+const { MongoClient } = require("mongodb");
+const config = require(".dbconfig.json");
+
+const url = `mongodb+srv://${config.username}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db("tuneshare-startup");
+const userCollection = db.collection("users");
+const songCollection = db.collection("songs");
+
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log("Connect to database");
+  } catch (ex) {
+    console.log(
+      `Unable to connect to database with ${url} because ${ex.message}`
+    );
+    process.exit(1);
+  }
+})();
+
+function getUser(username) {
+  return userCollection.findOne({ username: username });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function addUser(user) {
+  await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+  await userCollection.updateOne({ username: user.username }, { $set: user });
+}
+
+async function addSong(song) {
+  await songCollection.insertOne(song);
+}
+
+async function getSongs() {
+  return await songCollection.find().toArray();
+}
+
+module.exports = {
+  getUser,
+  getUserByToken,
+  addUser,
+  updateUser,
+  addSong,
+  getSongs,
+};
